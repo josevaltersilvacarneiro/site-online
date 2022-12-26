d601c54646bf3be1f90fe759946ba0fa7b1d5bd0
Jose Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2022-12-25 17:47:15 -0800

    The menu was added to the program

--- src/site-online.go
+++ src/site-online.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!")
+	var name string = "Jose Valter"
+	var version float32 = 1.1
+	var option int
+
+	fmt.Println("Hello", name)
+	fmt.Println("This program is in the version", version)
+
+	fmt.Println("1 - Start Monitoring")
+	fmt.Println("2 - Show logs")
+	fmt.Println("3 - Exit")
+
+	fmt.Scanf("%d", &option)
 }
